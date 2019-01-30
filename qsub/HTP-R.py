335b56587c11c244157dfd87d43f7856acd66331
ChenYen Lai <chengyanlai@gmail.com>
2019-01-30 15:54:38 -0800

    update htp-r parameters

--- qsub/HTP-R.py
+++ qsub/HTP-R.py
@@ -9,12 +9,12 @@ from Clusters import *
 
 dt = 0.005
 L = 3
-Nh = 20 * L
+Nh = 30 * L
 Prefix1 = "".join([ "L", str(L), "N", str(Nh) ])
 
-Omega, Lambda = 0.4, 1.0
+Omega, Lambda, Alpha = 0.4, 1.0, 2.0
 TSteps = np.int(200 * 6.5 * 10 / Omega)# Roughly 10 Phonon Period as 6.5 > 2 * pi
-Prefix2 = "-".join([ "".join(["W", str(Omega)]), "".join(["G", str(Lambda)]) ])
+Prefix2 = "-".join([ "".join(["W", str(Omega)]), "".join(["G", str(Lambda)]), str(Alpha) ])
 
 APPs = []
 APPs.append(os.path.join(SrcDir, "build", "holstein.r 1 Z 0"))
@@ -22,7 +22,7 @@ APPs.append("/bin/touch DONE")
 
 DataDir = os.path.join(ExecDir, "ED", "HTP-R", Prefix1, Prefix2)
 
-for cnt in range(1, 40, 1):
+for cnt in range(1001, 1002, 1):
     Prefix3 = "S"+str(cnt).zfill(4)
     workdir = os.path.join(DataDir, Prefix3)
 
@@ -35,8 +35,10 @@ for cnt in range(1, 40, 1):
     dset = g.create_dataset("N", data=Nh)
     dset = g.create_dataset("W", data=Omega)
     dset = g.create_dataset("G", data=Lambda)
-    AlphaReal = np.random.normal(2., 0.2, L)
-    AlphaPhase = np.random.uniform(-np.pi, np.pi, L)
+    # AlphaReal = np.random.normal(2., 0.2, L)
+    # AlphaPhase = np.random.uniform(-np.pi, np.pi, L)
+    AlphaReal = np.ones(L, dtype=np.float64) * Alpha
+    AlphaPhase = np.linspace(0., 2.*np.pi, L, endpoint=0)
     dset = g.create_dataset("AlphaReal", data=AlphaReal)
     dset = g.create_dataset("AlphaPhase", data=AlphaPhase)
     dset = g.create_dataset("TSteps", data=TSteps)
@@ -49,4 +51,4 @@ for cnt in range(1, 40, 1):
     elif Cluster == "Merced":
         sg.GenerateScript("TORQUE", Filename, JobName, APPs, workdir, Nodes=1, NumCore=20, WallTime='336:00:00', Partition='', ProjectName='', MPI=0, PPN=1)
     elif Cluster == "LANL":
-        sg.GenerateScript("SLURM", Filename, JobName, APPs, workdir, Nodes=1, NumCore=18, WallTime='16:00:00', Partition='standard', ProjectName='w18_xasrixs', MPI=1, PPN=1)
+        sg.GenerateScript("SLURM", Filename, JobName, APPs, workdir, Nodes=1, NumCore=36, WallTime='16:00:00', Partition='standard', ProjectName='s17_cint', MPI=0, PPN=1)
